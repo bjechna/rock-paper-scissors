0b196406fe3b92ac2fddb63bee00af06b21bcd4c
Błażej Jechna <blazejjechna7@gmail.com>
2024-04-20 15:14:17 +0200

    Added choosing card by enemy

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const play = document.getElementById("play");
 const enemyCard = document.querySelector(".enemy-card");
+const enemyBackCard = document.querySelector(".back")
 const playerCards = Array.prototype.slice.call(document.querySelector(".player-cards-container").children, 0); // Turning collection into array
-let active;
+
+let playerChoose;
+let enemyChoose;
+let enemyScore = 0;
+let playerScore = 0;
 
 playerCards.forEach(child => {
     child.addEventListener("click", function() {setActive(child)});
@@ -10,6 +15,8 @@ playerCards.forEach(child => {
 play.addEventListener("click", showCard);
 
 function showCard() {
+    drawEnemyCard();
+
     enemyCard.style.transform = "rotateY(180deg)";
 }
 
@@ -20,6 +27,14 @@ function setActive(item) {
         }
     })
 
+    playerChoose = item.classList[1];
     item.classList.toggle("active");
-    active = item;
+}
+
+function drawEnemyCard() {
+    const options = ['paper', 'scissors', 'rock'];
+
+    enemyChoose = options[Math.floor(Math.random() * 3)];
+
+    enemyBackCard.style.background = `url(img/${enemyChoose}.png)`;
 }
